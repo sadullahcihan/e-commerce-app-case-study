447e18d864a0448d3230fb86256eea6427fc0f9b
sadullahcihan <sadullah.cihan@gmail.com>
2024-12-19 17:03:04 +0300

    +

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,21 +1,23 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { removeFromCart, updateQuantity, clearCart } from '../redux/cartSlice';
-import './Cart.css';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeFromCart, updateQuantity, clearCart } from "../redux/cartSlice";
+import "./Cart.css";
 
 const Cart = () => {
   const dispatch = useDispatch();
   const cartItems = useSelector((state) => state.cart.items);
-// Sepeti localStorage ile senkronize et
+
+  // Sepeti localStorage ile senkronize et
   useEffect(() => {
-  const saveToLocalStorage = () => {
-    localStorage.setItem('cart', JSON.stringify(cartItems));
-  };
-  
-  const timeoutId = setTimeout(saveToLocalStorage, 500); // 500ms debounce
-  
-  return () => clearTimeout(timeoutId); // Önceki işlemi temizler
-}, [cartItems]);
+    const saveToLocalStorage = () => {
+      localStorage.setItem("cart", JSON.stringify(cartItems));
+    };
+
+    const timeoutId = setTimeout(saveToLocalStorage, 500); // 500ms debounce
+
+    return () => clearTimeout(timeoutId); // Önceki işlemi temizler
+  }, [cartItems]);
+
   const handleRemove = (productId) => {
     dispatch(removeFromCart(productId));
   };
@@ -32,9 +34,15 @@ const Cart = () => {
     dispatch(clearCart());
   };
 
+  const calculateTotalPrice = () => {
+    return cartItems
+      .reduce((total, item) => total + item.price * item.quantity, 0)
+      .toFixed(2);
+  };
+
   return (
     <div className="cart">
-      <h2>Your Cart</h2>
+      <h3>Your Cart</h3>
       {cartItems.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
@@ -42,33 +50,43 @@ const Cart = () => {
           <ul>
             {cartItems.map((item) => (
               <li key={item.id} className="cart-item">
-                <img src={item.image} alt={item.name} className="cart-item-image" />
                 <div className="cart-item-details">
                   <h4>{item.name}</h4>
                   <p>Price: {item.price} $</p>
                   <div className="quantity-control">
                     <button
-                      onClick={() => handleQuantityChange(item.id, item.quantity - 1)}
+                      onClick={() =>
+                        handleQuantityChange(item.id, item.quantity - 1)
+                      }
                     >
                       -
                     </button>
                     <span>{item.quantity}</span>
                     <button
-                      onClick={() => handleQuantityChange(item.id, item.quantity + 1)}
+                      onClick={() =>
+                        handleQuantityChange(item.id, item.quantity + 1)
+                      }
                     >
                       +
                     </button>
                   </div>
                 </div>
-                <button className="remove-button" onClick={() => handleRemove(item.id)}>
+                <button
+                  className="remove-button"
+                  onClick={() => handleRemove(item.id)}
+                >
                   Remove
                 </button>
               </li>
             ))}
           </ul>
-          <button className="clear-cart-button" onClick={handleClearCart}>
-            Clear Cart
-          </button>
+          <div className="total-card">
+            <h4>Total Price: {calculateTotalPrice()} $
+            <button className="checkout-button" onClick={handleClearCart}>
+              Clear Cart
+            </button></h4>
+            <button className="clear-cart-button">Checkout</button>
+          </div>
         </div>
       )}
     </div>
