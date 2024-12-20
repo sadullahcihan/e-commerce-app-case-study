46207b008843a9434bf91fec023462e97e261e34
sadullahcihan <sadullah.cihan@gmail.com>
2024-12-20 15:59:37 +0300

    +

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -40,72 +40,85 @@ const Cart = () => {
   };
 
   return (
-    <Card title={<Title level={4}>Your Cart</Title>}>
-      {cartItems.length === 0 ? (
-        <Text>Your cart is empty.</Text>
-      ) : (
-        <div>
-          <List
-            dataSource={cartItems}
-            renderItem={(item) => (
-              <List.Item key={item.id} className="cart-item">
-                <Space direction="vertical" className="cart-item-details">
-                  <Space
-                    direction="horizontal"
-                    className="item-header"
-                    justify="space-between"
-                  >
-                    <Title level={5}>{item.name}</Title>
-                    <Button
-                      onClick={() =>
-                        handleQuantityChange(item.id, item.quantity - 1)
-                      }
+    <>
+      <Card
+        title={<Title level={4}>Your Cart</Title>}
+        style={{ marginBottom: "10px" }}
+      >
+        {cartItems.length === 0 ? (
+          <Text>Your cart is empty.</Text>
+        ) : (
+          <div>
+            <List
+              dataSource={cartItems}
+              renderItem={(item) => (
+                <List.Item key={item.id} className="cart-item">
+                  <Space direction="vertical" className="cart-item-details">
+                    <Space
+                      direction="horizontal"
+                      className="item-header"
+                      justify="space-between"
                     >
-                      -
-                    </Button>
-                    <Badge
-                      count={item.quantity}
-                      style={{
-                        backgroundColor: "blue",
-                        color: "white",
-                        fontSize: "16px", // Yazı boyutu
-                        height: "30px", // Yükseklik
-                        width: "30px", // Genişlik
-                        lineHeight: "30px", // Dikey ortalama
-                        borderRadius: "0", // Köşeler kare olacak
-                        textAlign: "center", // Yatay ortalama
-                      }}
-                    />
-
-                    <Button
-                      onClick={() =>
-                        handleQuantityChange(item.id, item.quantity + 1)
-                      }
-                    >
-                      +
-                    </Button>
+                      <Title level={5}>{item.name}</Title>
+                      <Button
+                        onClick={() =>
+                          handleQuantityChange(item.id, item.quantity - 1)
+                        }
+                      >
+                        -
+                      </Button>
+                      <Badge
+                        count={item.quantity}
+                        style={{
+                          backgroundColor: "blue",
+                          color: "white",
+                          fontSize: "16px", // Yazı boyutu
+                          height: "30px", // Yükseklik
+                          width: "30px", // Genişlik
+                          lineHeight: "30px", // Dikey ortalama
+                          borderRadius: "0", // Köşeler kare olacak
+                          textAlign: "center", // Yatay ortalama
+                        }}
+                      />
+                      <Button
+                        onClick={() =>
+                          handleQuantityChange(item.id, item.quantity + 1)
+                        }
+                      >
+                        +
+                      </Button>
+                    </Space>
+                    <Space className="quantity-control">
+                      <Text>{item.price} $</Text>
+                    </Space>
                   </Space>
-                  <Space className="quantity-control">
-                    <Text>{item.price} $</Text>
-                  </Space>
-                </Space>
-              </List.Item>
-            )}
-          />
-          <div className="total-card">
-            <Space direction="vertical" size="middle">
-              <Text strong>Total Price: {calculateTotalPrice()} $</Text>
-              <Space>
-                <Button type="primary" onClick={handleClearCart}>
-                  Clear Cart
-                </Button>
-                <Button type="default">Checkout</Button>
-              </Space>
-            </Space>
+                </List.Item>
+              )}
+            />
           </div>
-        </div>
-      )}
-    </Card>
+        )}
+        <Button type="primary" onClick={handleClearCart}>
+          Clear Cart
+        </Button>
+      </Card>
+
+      <Card style={{ marginTop: "10px" }}>
+        <Space
+          direction="vertical"
+          size="middle"
+          style={{ width: "100%", alignContent: "center" }}
+        >
+          <Text strong>
+            <span style={{ color: "black" }}>Total Price: </span>
+            <span style={{ color: "#007bff" }}>{calculateTotalPrice()} $</span>
+          </Text>
+
+          <Button type="primary" size="large">
+            Checkout
+          </Button>
+        </Space>
+      </Card>
+    </>
   );
 };
 
